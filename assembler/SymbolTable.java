7f3bbdd1d1ffb32e2bfe98096370c2aa70f4fdb2
Taylor King <kingtb@appstate.edu>
2016-01-26 18:57:44 -0500

    Add symbol table

---
+++ assembler/SymbolTable.java
@@ -0,0 +1,12 @@
+public class SymbolTable {
+  private static SymbolTable instance;
+  public SymbolTable() {
+  
+    }
+  public static SymbolTable getInstance() {
+    if(instance == null) {
+      instance = new SymbolTable();
+    }
+    return instance;
+  }
+}
