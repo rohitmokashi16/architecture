6acd359fc4f5f93eeb616614b1e2752f740acdcd
Taylor King <kingtb@appstate.edu>
2016-01-26 19:22:24 -0500

    Symbol table stuff

--- assembler/SymbolTable.java
+++ assembler/SymbolTable.java
@@ -1,12 +1,30 @@
+import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+
+
 public class SymbolTable {
+  
+  private HashMap<String, Integer> map;
   private static SymbolTable instance;
+  
   public SymbolTable() {
-    
+      map = new HashMap<>();
   }
+
   public static SymbolTable getInstance() {
     if(instance == null) {
       instance = new SymbolTable();
     }
     return instance;
+  }
+
+  public void readFile(String filename) {
+  
+  }
+
+  public int lookupSymbol(String symbolName) {
+    return map.get(symbolName);
   }
 }
